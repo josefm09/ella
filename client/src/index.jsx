9f086dd3fc052cb76b8faf95f8b7d4e41be669fb
Shane <31248360+shanedle@users.noreply.github.com>
2022-08-28 19:25:32 +0200

    :arrow_down: downgraded react

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,18 +1,18 @@
 import "./index.css";
 
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
 import App from "./App";
 import store from "./store";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
+  document.getElementById("root")
 );
 
 reportWebVitals();
